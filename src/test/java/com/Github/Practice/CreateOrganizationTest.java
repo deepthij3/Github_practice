167c3d89ff939f72bfd813fbc8b0d666a910c590
jagad <jagad@jadi>
2021-12-14 12:14:26 +0530

    New Project Added

---
+++ src/test/java/com/Github/Practice/CreateOrganizationTest.java
@@ -0,0 +1,9 @@
+package com.Github.Practice;
+
+public class CreateOrganizationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Add the Organizations");
+	}
+
+}
---
+++ src/test/java/com/Github/Practice/LoginPageTest.java
@@ -0,0 +1,9 @@
+package com.Github.Practice;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		System.out.println("Give the Credentials");
+	}
+
+}
